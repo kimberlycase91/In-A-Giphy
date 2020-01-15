052b4aeac40e9e83c20d5656fa3d57d1898d77da
kimberlycase91 <kimberlycase91@gmail.com>
2020-01-15 15:01:57 -0700

    pseudocode in app.js, added basic html and linked to app.js and jquery

---
+++ assets/app.js
@@ -0,0 +1,30 @@
+//array of items
+//variable to store item clicked on
+//variable to store API key
+
+//on page load, loop through array
+
+    //create a button for each item in the array
+
+//when one of theses buttons is clicked 
+
+    //put that button into a variable that will be used in the urlQuery
+    //run urlquery function
+    //display 10 results 
+
+//function for giphy api url
+
+    //takes in the value of the item clicked on
+    //returns 10 related gifs from giphy API (image and animated urls), and their rating
+    //displays these as images with their rating
+
+//when a gif is clicked on
+
+    //toggle between image and animation
+
+//for add an item: on submint (event.preventDefault())
+
+    //push trimmed value of item into the array
+    //make sure this creates a button automatically??
+
+    
