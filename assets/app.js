1379dfa132e36691c58c55d8a7de31d00d851d84
kimberlycase91 <kimberlycase91@gmail.com>
2020-01-16 14:41:14 -0700

    added on-click event to place value of button clicked into a variable

--- assets/app.js
+++ assets/app.js
@@ -1,5 +1,4 @@
 var topics = ["happy", "sad", "angry", "tired", "hungry"];  //array of topics
-var topic //variable to store topic clicked on
 var API_KEY = "YjMawClttmBnYiW3mWkkZVDiwISuNBet" //variable to store API key
 var limit = 10; //limits the response to ten gifs to display
 
@@ -9,17 +8,35 @@ $(document).ready(function(){
 //create a button for each topic in the array
 for (var i=0; i < topics.length; i++){
 var newBtn = $("<button>");
-newBtn.attr("id", "topicButton");
+newBtn.addClass("topicButton");
+newBtn.attr("data-name", topics[i])
 newBtn.text(topics[i]);
 $("#button-div").append(newBtn);
 }
 });
 
 //when one of theses buttons is clicked 
+$(document).on("click", ".topicButton", function(){
     //put that button into a variable that will be used in the urlQuery
-    //run urlquery function
-    //display 10 results 
 
+    var topic = $(this).attr("data-name");
+    console.log(topic);
+    //run urlquery function
+    $.ajax({
+        url: queryURL(),
+        method: "GET"
+    }).then(function(response){
+    //create display div
+    var 
+    //creating rating variable
+    //create an elent to store rating
+    //display rating
+    //display 10 results    
+    //create an element to hold gif/image
+    //append the image to display div
+    })
+    
+})
 //function for giphy api url
 function queryURL () {
     var baseURL = 'https://api.giphy.com/v1/gifs/search?';
@@ -36,7 +53,6 @@ function queryURL () {
 
     return baseURL
 }
-console.log(queryURL(topic="sad"))
     //takes in the value of the item clicked on
     //returns 10 related gifs from giphy API (image and animated urls), and their rating
     //displays these as images with their rating
