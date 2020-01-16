2440b0c5ca972d53a2849838c46fdaa8e436a244
kimberlycase91 <kimberlycase91@gmail.com>
2020-01-16 12:32:07 -0700

    created function to generate queryURL

--- assets/app.js
+++ assets/app.js
@@ -1,19 +1,38 @@
-//array of items
-//variable to store item clicked on
-//variable to store API key
+var topics = ["happy", "sad", "angry", "tired", "hungry"];  //array of topics
+var topic //variable to store topic clicked on
+var API_KEY = "YjMawClttmBnYiW3mWkkZVDiwISuNBet" //variable to store API key
+var limit = 10; //limits the response to ten gifs to display
 
 //on page load, loop through array
+// $(document).ready(function(){
 
-    //create a button for each item in the array
-
+// //create a button for each topic in the array
+// $.each(topics, function(index, value) {
+// var newBtn = 
+// })
 //when one of theses buttons is clicked 
 
     //put that button into a variable that will be used in the urlQuery
     //run urlquery function
     //display 10 results 
-
+// })
 //function for giphy api url
+function queryURL () {
+    var baseURL = 'https://api.giphy.com/v1/gifs/search?';
 
+    baseURL += 'q=' + topic;
+
+    baseURL += '&limit=' + limit;
+
+    // baseURL += '&rating=' + rating;
+    
+    var API_KEY = "YjMawClttmBnYiW3mWkkZVDiwISuNBet";
+     
+    baseURL += '&api_key=' + API_KEY;
+
+    return baseURL
+}
+console.log(queryURL(topic="sad"))
     //takes in the value of the item clicked on
     //returns 10 related gifs from giphy API (image and animated urls), and their rating
     //displays these as images with their rating
