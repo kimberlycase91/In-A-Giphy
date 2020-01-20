cecd86af9cf70ec1532e598768e55f6c7958d973
kimberlycase91 <kimberlycase91@gmail.com>
2020-01-19 21:53:42 -0700

    set up add item to push to array and render buttons on the page-set up on click of the gifs to toggle between still and animated

--- assets/app.js
+++ assets/app.js
@@ -1,44 +1,64 @@
 var topics = ["happy", "sad", "angry", "tired", "hungry"];  //array of topics
-var API_KEY = "YjMawClttmBnYiW3mWkkZVDiwISuNBet" //variable to store API key
 var limit = 10; //limits the response to ten gifs to display
 
-//on page load, loop through array
-$(document).ready(function(){
-
-//create a button for each topic in the array
-for (var i=0; i < topics.length; i++){
-var newBtn = $("<button>");
-newBtn.addClass("topicButton");
-newBtn.attr("data-name", topics[i])
-newBtn.text(topics[i]);
-$("#button-div").append(newBtn);
-}
+//on page load, run renderButtons function
+$(document).ready(function () {
+    renderButtons();
 });
 
-//when one of theses buttons is clicked 
-$(document).on("click", ".topicButton", function(){
-    //put that button into a variable that will be used in the urlQuery
+ //function to render buttons, using a for loop to create a button for each topic in the array
+ function renderButtons (){
+    $("#button-div").empty();
 
-    var topic = $(this).attr("data-name");
+    for (var i = 0; i < topics.length; i++) {
+        var newBtn = $("<button>");
+        newBtn.addClass("topicButton");
+        newBtn.attr("data-name", topics[i])
+        newBtn.text(topics[i]);
+        $("#button-div").append(newBtn);
+    }}
+
+//when one of theses buttons is clicked 
+$(document).on("click", ".topicButton", function () {
+    $(".resultsContainer").empty();
+    //put that button into a variable that will be used in the urlQuery
+    var input = $(this).attr("data-name");
+    topic = input;
     console.log(topic);
     //run urlquery function
     $.ajax({
         url: queryURL(),
         method: "GET"
-    }).then(function(response){
-    //create display div
-    var 
-    //creating rating variable
-    //create an elent to store rating
-    //display rating
-    //display 10 results    
-    //create an element to hold gif/image
-    //append the image to display div
-    })
+    }).then(function (response) {
+        //create display div
+        var results = response.data;
+        var resultsDiv = $("<div class='results-div'>");
+        //loop through the query results
+        for (var i = 0; i < results.length; i++) {
+            //creating rating variable
+            var rating = results[i].rating;
+            //create an element to store the rating
+            var ratingP = $("<p>").text("Rated: " + rating);
+            //display the rating
+            resultsDiv.append(ratingP);
+            //create a variable to pull image url
+            var imageURL = results[i].images.fixed_height_still.url;
+            //create an element to hold gif/image
+            var image = $("<img>").attr("src", imageURL);
+            image.attr("id", "gif")
+            image.attr("data-state", "still")
+            image.attr("data-animate", results[i].images.fixed_height.url);
+            image.attr("data-still", results[i].images.fixed_height_still.url);
+            //append the image to display div
     
+            resultsDiv.append(image);
+            $(".resultsContainer").append(resultsDiv);
+        }
+    })
+
 })
 //function for giphy api url
-function queryURL () {
+function queryURL() {
     var baseURL = 'https://api.giphy.com/v1/gifs/search?';
 
     baseURL += 'q=' + topic;
@@ -46,24 +66,45 @@ function queryURL () {
     baseURL += '&limit=' + limit;
 
     // baseURL += '&rating=' + rating;
-    
+
     var API_KEY = "YjMawClttmBnYiW3mWkkZVDiwISuNBet";
-     
+
     baseURL += '&api_key=' + API_KEY;
 
     return baseURL
+    console.log(baseURL);
+    
 }
-    //takes in the value of the item clicked on
-    //returns 10 related gifs from giphy API (image and animated urls), and their rating
-    //displays these as images with their rating
+   
+//when a topic is added
+$("#add-item").on("submit", function (event) {
+    event.preventDefault();
+    //create a variable to store the new item
+    var topic = $("#item-input").val().trim();
+    //push trimmed value of item into the array
+    topics.push(topic);
+    //make sure this creates a button automatically??
+    renderButtons();
+});
+
+console.log(queryURL(topic="happy"))
 
 //when a gif is clicked on
+$(document).on("click", "#gif", function() {
+    console.log($(this));
 
-    //toggle between image and animation
-
-//for add an item: on submint (event.preventDefault())
-
-    //push trimmed value of item into the array
-    //make sure this creates a button automatically??
-
+    // The attr jQuery method allows us to get or set the value of any attribute on our HTML element
+    var state = $(this).attr("data-state");
     
+    // If the clicked image's state is still, update its src attribute to what its data-animate value is.
+    // Then, set the image's data-state to animate
+    // Else set src to the data-still value
+    if (state === "still") {
+      $(this).attr("src", $(this).attr("data-animate"));
+      $(this).attr("data-state", "animate");
+    } else {
+      $(this).attr("src", $(this).attr("data-still"));
+      $(this).attr("data-state", "still");
+    }
+  });
+
